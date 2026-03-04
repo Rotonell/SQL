df5af7a61769e99cabdfd4e343a349c1f476db1f
Дарья Серова <dashaflay2000@gmail.com>
2026-03-04 20:31:08 +0300

    GetNextLearningDate

--- Functions/SQLQuery0 - CHECK.sql
+++ Functions/SQLQuery0 - CHECK.sql
@@ -2,4 +2,6 @@ USE PV_521_Import
 SET DATEFIRST 1;
 
 EXEC sp_SelectSheduleFor N'PV_521'
-PRINT dbo.GetNextLearningDay (N'PV_521')
+--PRINT dbo.GetNextLearningDay(N'PV_521', DEFAULT);
+PRINT dbo.GetNextLearningDay(N'PV_521', N'2026-03-05');
+PRINT dbo.GetNextLearningDay(N'PV_521', N'2026-03-05');
